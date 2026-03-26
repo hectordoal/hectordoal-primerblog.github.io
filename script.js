979d733d6a6b5131e9cb68eea412d87046243957
hectordoal <hectordoal23@gmail.com>
2026-03-26 19:03:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     
-    // Ahora buscamos solo las imágenes que tienen la clase "foto-indice"
+    //buscamos solo las imágenes que tienen la clase "foto-indice"
     const fotosIndice = document.querySelectorAll(".foto-indice");
 
     fotosIndice.forEach(function(img) {
         img.addEventListener("click", function() {
-            alert("¡Esta receta tiene tiene muy buena pinta! ¿Te animas a cocinarla?");
+           
+           const respuesta = confirm("Esta receta tiene buena pinta, ¿te animas a cocinarla?");
+
+           if (respuesta === true) {
+                // Si pulsa "Aceptar" (Sí), buscamos el enlace que hay justo debajo de la foto
+                const enlace = img.parentElement.querySelector("a").href;
+                window.location.href = enlace; // Nos lleva a la receta
+            } else {
+                // Si pulsa "Cancelar" (No), no hace nada
+                console.log("El usuario ha decidido quedarse en el índice.");
+            }
         });
     });
 });
